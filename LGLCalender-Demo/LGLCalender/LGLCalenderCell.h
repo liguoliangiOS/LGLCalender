3b24d2c7d29d00b64ddede9ff460b6a2950e9380
liguoliang <123@qq.com>
2016-10-12 12:07:53 +0800

    add

--- LGLCalender-Demo/LGLCalender/LGLCalenderCell.h
+++ LGLCalender-Demo/LGLCalender/LGLCalenderCell.h
@@ -4,7 +4,7 @@
 //
 //  Created by 李国良 on 2016/10/11.
 //  Copyright © 2016年 李国良. All rights reserved.
-//
+//  https://github.com/liguoliangiOS/LGLCalender.git
 
 #import <UIKit/UIKit.h>
 
--- LGLCalender-Demo/LGLCalender/LGLWeekView.m
+++ LGLCalender-Demo/LGLCalender/LGLWeekView.m
@@ -4,7 +4,7 @@
 //
 //  Created by 李国良 on 2016/10/12.
 //  Copyright © 2016年 李国良. All rights reserved.
-//
+//  https://github.com/liguoliangiOS/LGLCalender.git
 
 #import "LGLWeekView.h"
 
